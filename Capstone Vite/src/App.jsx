5c8f520f35748c956cc76b101aae27a612eaedee
Ivan-Jose-Perez <ivan.jose@ivanjp.tech>
2024-04-10 19:05:05 -0700

    Debugging to show site: fxed directories and imports

--- Capstone Vite/src/App.jsx
+++ Capstone Vite/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import ItemDetails from './components/ItemDetails';
-import SearchResults from './components/SearchResults';
-import UserProfile from './components/UserProfile';
-import AdminDashboard from './components/AdminDashboard';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "../components/HomePage";
+import ItemDetails from '../components/ItemDetails';
+import SearchResults from '../components/SearchResults';
+import UserProfile from '../components/UserProfile';
+import AdminDashboard from '../components/AdminDashboard';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/details/:id" component={ItemDetails} />
-        <Route path="/search" component={SearchResults} />
-        <Route path="/profile" component={UserProfile} />
-        <Route path="/admin" component={AdminDashboard} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/details/:id" element={<ItemDetails />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </Router>
   );
 }
